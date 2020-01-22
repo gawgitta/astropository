600f8d0dd5219d4ac2352267e809b0ba2647ba1a
gawgitta <57604593+gawgitta@users.noreply.github.com>
2020-01-22 17:14:07 -0500

    Add files via upload

---
+++ astroGlobalNPC.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace astro.NPCs
+{
+	public class astroGlobalNPC : GlobalNPC
+	{
+		public override void NPCLoot(NPC npc)
+		{
+			if (npc.type == 35 || npc.type == 36 || npc.type == 222 || npc.type == 113) //Skeletron  Hands, Skeletron, Queen Bee, or WoF
+			{
+				Item.NewItem(npc.getRect(), mod.ItemType("ISOre"), Main.rand.Next(45, 95));
+			} 
+		}
+	}
+
+}
